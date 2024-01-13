7c088f9a42266ce928fcd17ec3eb4a1a839c96f6
Askhad Arzybaev <aarzybaev@gmail.com>
2024-01-13 12:18:17 +0600

    created constants file

---
+++ src/container/App/constants.ts
@@ -0,0 +1,14 @@
+import {FastfoodItem} from '../../type';
+import burgerImg from '../../assets/burger.png';
+import friedImg from '../../assets/fried.png';
+import coffeeImg from '../../assets/coffee.png';
+import teaImg from '../../assets/tea.png';
+import colaImg from '../../assets/cola.png';
+export const FASTFOOD_ITEMS:FastfoodItem[] = [
+  {name: 'Hamburger', price: 80, image: burgerImg},
+  {name: 'Cheeseburger', price: 90, image: burgerImg},
+  {name: 'Fries', price: 45, image: friedImg},
+  {name: 'Coffee', price: 70, image: coffeeImg},
+  {name: 'Tea', price: 50, image: teaImg},
+  {name: 'Cola', price: 40, image: colaImg}
+];
